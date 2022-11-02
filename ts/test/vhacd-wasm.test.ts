b32bdeea1a9f5f0e502ea61cc24cc53c5a1dd5bf
pmconne <22944042+pmconne@users.noreply.github.com>
2022-11-02 16:14:28 -0400

    rename test file.

---
+++ ts/test/vhacd-wasm.test.ts
@@ -0,0 +1,30 @@
+
+import { expect } from "chai";
+import instantiateModule from "../../lib/vhacd-wasm.js";
+import { VHACD } from "../vhacd-wasm";
+
+describe("test", async () => {
+  let vhacd: typeof VHACD;
+  before(async () => {
+    vhacd = await instantiateModule();
+  });
+
+  it("Parameters", () => {
+    const params = new vhacd.Parameters();
+    expect(params.maxHulls).to.equal(64);
+    params.maxHulls = 32;
+    expect(params.maxHulls).to.equal(32);
+  });
+
+  it("vector", () => {
+    const params = new vhacd.Parameters();
+    const decomposer = new vhacd.MeshDecomposer(params);
+    const hulls = decomposer.compute(0, 0, 0, 0);
+    expect(hulls.size()).to.equal(0);
+    expect(hulls.get(1)).to.be.undefined;
+  });
+
+  it("heap", () => {
+    expect(vhacd.HEAPU8).instanceof(Uint8Array);
+  });
+});
