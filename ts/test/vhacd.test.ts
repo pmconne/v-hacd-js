7548b4588e24b7ac84287038e50328c7b9a3e9b9
pmconne <22944042+pmconne@users.noreply.github.com>
2022-11-02 14:52:12 -0400

    A test that actually tests something.

--- ts/test/vhacd.test.ts
+++ ts/test/vhacd.test.ts
@@ -1,8 +1,22 @@
 
 import { expect } from "chai";
+import instantiateModule from "../../lib/vhacd-wasm.js";
+import { Parameters } from "../vhacd.js";
 
-describe("test", () => {
+describe("test", async () => {
+  let module;
+  before(async () => {
+    module = await instantiateModule();
+  });
+
   it("test", () => {
-    expect(false).to.be.true;
+    expect(module.HEAPU8).not.to.be.undefined;
+  });
+
+  it("Parameters", () => {
+    const params = new module.Parameters() as Parameters;
+    expect(params.maxHulls).to.equal(64);
+    params.maxHulls = 32;
+    expect(params.maxHulls).to.equal(32);
   });
 });
