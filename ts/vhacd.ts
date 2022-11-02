54bc9960f3fa237d3d25b50b74e36af5f8b3214e
pmconne <22944042+pmconne@users.noreply.github.com>
2022-11-02 09:29:57 -0400

    Public TypeScript API.

--- ts/vhacd.ts
+++ ts/vhacd.ts
@@ -1,6 +1,30 @@
 
 import  * as wasm from "../lib/vhacd-wasm.js";
 
+export type HullFillMode = "flood" | "surface" | "raycast";
+
+export interface Parameters {
+  maxHulls?: number;
+  voxelResolution?: number;
+  minVolumePercentError?: number;
+  maxRecursionDepth?: number;
+  shrinkWrap?: boolean;
+  fillMode?: HullFillMode;
+  maxVerticesPerHull?: number;
+  minEdgeLength?: number;
+  findBestPlane?: boolean;
+}
+
+export interface Mesh {
+  positions: Float64Array;
+  indices: Uint32Array;
+}
+
+export function decomposeMesh(mesh: Readonly<Mesh>, parameters?: Parameters): Mesh[] {
+  // ###TODO instantiate VHACD and compute
+  return [];
+}
+
 console.log(wasm);
 export function test(arg: string) {
   console.log(arg);
