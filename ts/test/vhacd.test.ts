04be2c3cb20d9f668b55373fe850e9d737381c2b
pmconne <22944042+pmconne@users.noreply.github.com>
2022-11-03 10:59:09 -0400

    test maxVerticesPerHull

--- ts/test/vhacd.test.ts
+++ ts/test/vhacd.test.ts
@@ -9,20 +9,8 @@ describe("ConvexMeshDecomposition", async () => {
     decomposer = await ConvexMeshDecomposition.create();
   });
 
-  it("produces 1 hull for a triangle", () => {
-    const positions = new Float64Array([
-      0, 0, 0,
-      0, 1, 0,
-      1, 1, 0,
-    ]);
-
-    const indices = new Uint32Array([0, 1, 2]);
-    const hulls = decomposer.computeConvexHulls({ positions, indices });
-    expect(hulls.length).to.equal(1);
-  });
-
-  it("produces 1 hull for a cube", () => {
-    const positions = new Float64Array([
+  const cube = {
+    positions: new Float64Array([
       // Front face
       -1.0, -1.0,  1.0,
       1.0, -1.0,  1.0,
@@ -58,18 +46,33 @@ describe("ConvexMeshDecomposition", async () => {
       -1.0, -1.0,  1.0,
       -1.0,  1.0,  1.0,
       -1.0,  1.0, -1.0,
-    ]);
-
-    const indices = new Uint32Array([
+    ]),
+    indices: new Uint32Array([
       0,  1,  2,      0,  2,  3,    // front
       4,  5,  6,      4,  6,  7,    // back
       8,  9,  10,     8,  10, 11,   // top
       12, 13, 14,     12, 14, 15,   // bottom
       16, 17, 18,     16, 18, 19,   // right
       20, 21, 22,     20, 22, 23,   // left
-    ]);
+    ]),
+  };
 
-    const hulls = decomposer.computeConvexHulls({ positions, indices });
+  it("produces 1 hull for a cube", () => {
+    const hulls = decomposer.computeConvexHulls(cube);
     expect(hulls.length).to.equal(1);
-  }).timeout(0);
-});
+
+    const hull = hulls[0];
+    expect(hull.positions.length).to.equal(3 * 8);
+    expect(hull.indices.length).to.equal(3 * 12);
+    // ###TODO verify mesh
+  });
+
+  it("limits the number of vertices", () => {
+    const hulls = decomposer.computeConvexHulls(cube, { maxVerticesPerHull: 6 });
+    expect(hulls.length).to.equal(1);
+
+    const hull = hulls[0];
+    expect(hull.positions.length).to.equal(3 * 6);
+    expect(hull.indices.length).to.equal(3 * 8);
+  });
+}).timeout(0);
